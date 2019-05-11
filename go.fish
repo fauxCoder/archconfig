acb2b97957918258f4d18e7e4a180c1ab8a63b35
Jameson York <jamesonyork8@gmail.com>
2019-05-11 13:02:56 +1000

    Adds more.

--- go.fish
+++ go.fish
@@ -1,4 +1,23 @@
-#!/usr/bin/env fish
+#!/bin/bash
 
 # I'm guessing you've got git already.
 # You'll need fish too.
+
+# Update
+pacman -Syu
+
+# Reqs.
+pacman -S \
+python \
+rust \
+vim \
+xmonad \
+xmonad-contrib
+
+# Copy configs
+./blow.fish
+
+# YCM install
+pushd ~/.vim/plugged/YouCompleteMe/
+./install.py --clang-completer --rust-completer --system-libclang
+popd
