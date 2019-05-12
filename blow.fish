a65c83867da2ed30e0a5ddb4fdfb2c74ee823ffd
Jameson York <jamesonyork8@gmail.com>
2019-05-12 18:26:18 +1000

    Proper blow script.

--- blow.fish
+++ blow.fish
@@ -1,3 +1,11 @@
 #!/usr/bin/env fish
 
-echo yo
+pushd home
+set home_files (find . -type f)
+popd
+
+for f in $home_files
+    set file (echo $f | string replace ./ "")
+    mkdir -p ~/(dirname $file)
+    cp home/$file ~/$file
+end
